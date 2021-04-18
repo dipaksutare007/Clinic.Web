37d5ac7c341cd8a7b589be2081dffd6c58bc037a
dipak <dipaksutare007@gmail.com>
2021-04-18 04:33:04 -0700

    Add project files.

---
+++ Clinic.EF/ClinicDAL.cs
@@ -0,0 +1,26 @@
+﻿using Clinic.EF.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.EF
+{
+    public class ClinicDAL : DbContext
+    {
+        public ClinicDAL():base("name=ClinicDAL")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Patient>().ToTable("Patient");
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
---
+++ Clinic.EF/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Clinic.EF.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Clinic.EF.ClinicDAL>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Clinic.EF.ClinicDAL context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
