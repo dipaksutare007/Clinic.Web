e1b86623a794c0f930576ccdd1b61dbba64464a8
dipak <dipaksutare007@gmail.com>
2021-04-18 19:18:25 -0700

    New Project Repository Created

---
+++ Clinic.Core/Repository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clinic.ICore;
+using Clinic.EF;
+using System.Data.Entity;
+
+namespace Clinic.Core
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private ClinicDAL db=null;
+        private IDbSet<T> dbentity=null;
+
+        public Repository()
+        {
+            db = new ClinicDAL();
+            dbentity = db.Set<T>();
+        }
+        public Repository(ClinicDAL _db)
+        {
+            db = _db;
+            dbentity = db.Set<T>();
+        }
+        public void Delete(T obj)
+        {
+            dbentity.Remove(obj);
+        }
+
+        public T GetbyID(object id)
+        {
+            return dbentity.Find(id);
+        }
+
+        public IEnumerable<T> Getdata()
+        {
+            return dbentity.ToList();
+        }
+
+        public void Insert(T obj)
+        {
+            dbentity.Add(obj);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Clinic.ICore/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.ICore
+{
+    public interface IRepository<T> where T:class
+    {
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(T obj);
+        IEnumerable<T> Getdata();
+        T GetbyID(object id);
+        void Save();
+
+    }
+}
